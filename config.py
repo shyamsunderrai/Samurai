20ab9bb74b753aacc5236e51343477a83a452f08
shyamsunderrai <whoisshyam@gmail.com>
2017-11-05 20:36:06 -0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 #	https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = "YkJgXwaWIbEtIKYRC3bN81g76"
-consumer_secret = "ZxCqhv5QJxjUkikdJgT9ldkVemcCT734JP0xl2M3pzVwP3PSZa"
-access_key = "53291862-y21VnQ0RiVdBqxv3oyVbpgHaCZ6Pzxn61gEKOWrbj"
-access_secret = "GJ6ztmfn0acEtt00p6VhlQN21GMGXhVRt4w5ky6CKSunw"
+consumer_key = "XXXX"
+consumer_secret = "xxx"
+access_key = "xxx"
+access_secret = "xxxx"
